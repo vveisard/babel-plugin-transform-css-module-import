76fd044bc9a732b791d773d193958ee3f77f6d09
Walter Blacke <walter.blacke@vegabyte.studio>
2023-10-07 11:50:26 -0400

    add code

---
+++ deps.ts
@@ -0,0 +1,14 @@
+export { type PluginObj, type PluginPass } from "https://esm.sh/@babel/core@7.22.20";
+export {
+  identifier,
+  ObjectExpression,
+  objectExpression,
+  objectProperty,
+  stringLiteral,
+  variableDeclaration,
+  variableDeclarator,
+} from "https://esm.sh/@babel/types@7.22.19";
+export {
+  dirname,
+  join
+} from "https://deno.land/std@0.202.0/path/mod.ts";
---
+++ mod.ts
@@ -0,0 +1,149 @@
+import { PluginObj, PluginPass } from "./deps.ts";
+import {
+  identifier,
+  ObjectExpression,
+  objectExpression,
+  objectProperty,
+  stringLiteral,
+  variableDeclaration,
+  variableDeclarator,
+} from "./deps.ts";
+import { 
+  dirname,
+  join
+} from "./deps.ts";
+
+/**
+ * Exports of a CSS module.
+ * Record of selector to scoped (compiled) selector.
+ */
+interface CSSModuleExports {
+  /**
+   * key: selector.
+   * value: scoped (compiled) selector.
+   */
+  [selector: string]: string;
+}
+
+interface PluginOptions {
+  /**
+   * Path of the root directory, which all file paths are resolved relative to.
+   */
+  readonly absoluteRootDirPath: string;
+
+  /**
+   * @param absoluteCssFilePath path of the CSS module file, relative to the `rootDirPath`.
+   */
+  getCssModuleExports: (
+    absoluteRootDirPath: string,
+    state: PluginPass,
+    absoluteCssFilePath: string,
+  ) => CSSModuleExports;
+}
+
+/**
+ * Create an {@link ObjectExpression} from {@link CSSModuleExports}.
+ * @param cssModuleExports
+ * @returns
+ */
+function createObjectExpressionFromCssModuleExports(
+  cssModuleExports: CSSModuleExports,
+): ObjectExpression {
+  return objectExpression(
+    Object.keys(cssModuleExports).map((token) =>
+      objectProperty(
+        stringLiteral(token),
+        stringLiteral(cssModuleExports[token]),
+      )
+    ),
+  );
+}
+
+/**
+ * Check if the source of an import decleration matches the pattern of a css module..
+ */
+function importDeclarationSourceMatches(source: string) {
+  const parts = source.split(".");
+
+  return (parts.at(-1) === "css" && parts.at(-2) === "module");
+}
+
+/**
+ * Transforms CSS module import to a const declaration of that CSS module's exports.
+ * @example
+ * ```css
+ * // App.module.css
+ * .foo {
+ *   width: 100%;
+ *   height: 100%;
+ *   background-color: green;
+ * }
+ * ```
+ * Input
+ * ```jsx
+ * // App.jsx
+ * import styles from "./App.module.css";
+ * ```
+ *
+ * Output:
+ * ```jsx
+ * // App.jsx
+ * const styles = {
+ *   "foo": "wT-Mma_foo"
+ * }
+ * ```
+ */
+function plugin(
+  options: PluginOptions,
+): PluginObj {
+  return {
+    visitor: {
+      ImportDeclaration(path, state) {
+        if (path.node.specifiers.length !== 1) {
+          return;
+        }
+
+        const specifier = path.node.specifiers[0];
+
+        if (specifier.type !== "ImportDefaultSpecifier") {
+          return;
+        }
+
+        if (!importDeclarationSourceMatches(path.node.source.value)) {
+          return;
+        }
+
+        const absoluteSourceFilePath = state.filename;
+        const absoluteSourceDirPath = dirname(
+          absoluteSourceFilePath,
+        );
+        const absoluteCssFilePath = join(
+          absoluteSourceDirPath,
+          path.node.source.value!,
+        );
+
+        const cssModuleExports = options.getCssModuleExports(
+          options.absoluteRootDirPath,
+          state,
+          absoluteCssFilePath,
+        );
+
+        path.replaceWith(
+          variableDeclaration(
+            "const",
+            [
+              variableDeclarator(
+                identifier(specifier.local.name),
+                createObjectExpressionFromCssModuleExports(
+                  cssModuleExports,
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    },
+  };
+}
+
+export default plugin;
